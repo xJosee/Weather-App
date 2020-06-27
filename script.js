71eec9a5140211680b1d086ce7c35f8ff729ecda
xJosee <xJosee@github.com>
2020-06-27 02:32:54 -0600

    update

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ const api = {
   }
   
   function getResults (query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then(weather => {
-        return weather.json();
-      }).then(displayResults);
+    fetch(`${api.base}weather?q=${query}&appid=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
   }
   
   function displayResults (weather) {
@@ -31,7 +31,7 @@ const api = {
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
   
     let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
+    weather_el.innerText = weather.weather[0].description;
   
     let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
